91d272e3ca0c5c5c9ec0e708407811d1979ed34c
xinyuexy <1151980485@qq.com>
2018-10-27 10:35:25 +0800

    add bar chart

---
+++ static/js/barChart.js
@@ -0,0 +1,86 @@
+function drawBarChart(data) {
+    console.log(data);
+    d3.select("#somType").select("svg > *").remove();
+    var element = d3.select('#somType').node();
+    var div_width = element.getBoundingClientRect().width;
+    var div_height = element.getBoundingClientRect().height;
+
+    //sort bars based on value
+    data = data.sort(function (a, b) {
+        return d3.ascending(a.value, b.value);
+    });
+
+    //set up svg using margin conventions - we'll need plenty of room on the left for labels
+    var margin = {
+        top: 15,
+        right: 25,
+        bottom: 15,
+        left: 60
+    };
+
+    var width = div_width - margin.left - margin.right,
+        height = div_height - margin.top - margin.bottom;
+
+    var svg = d3.select("#somType").append("svg")
+        .attr("preserveAspectRatio", "xMidYMid meet")
+        .attr("viewBox", "0 0 "+div_width+ " "+div_height)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var x = d3.scale.linear()
+        .range([0, width])
+        .domain([0, d3.max(data, function (d) {
+            return d.value;
+        })]);
+
+    var y = d3.scale.ordinal()
+        .rangeRoundBands([height, 0], .1)
+        .domain(data.map(function (d) {
+            return d.name;
+        }));
+    //make y axis to show bar names
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        //no tick marks
+        .tickSize(0)
+        .orient("left");
+
+    var gy = svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
+    var bars = svg.selectAll(".bar")
+        .data(data)
+        .enter()
+        .append("g");
+
+    //append rects
+    bars.append("rect")
+        .attr("class", "bar")
+        .attr("y", function (d) {
+            return y(d.name);
+        })
+        .attr("height", y.rangeBand())
+        .attr("x", 0)
+        .attr("width", function (d) {
+            return x(d.value);
+        })
+        .attr("fill", function (d) {
+            return d.color;
+        });
+
+    //add a value label to the right of each bar
+    bars.append("text")
+        .attr("class", "label")
+        //y position of the label is halfway down the bar
+        .attr("y", function (d) {
+            return y(d.name) + y.rangeBand() / 2 + 4;
+        })
+        //x position is 3 pixels to the right of the bar
+        .attr("x", function (d) {
+            return x(d.value) + 3;
+        })
+        .text(function (d) {
+            return d.value;
+        });
+}
--- static/js/color.js
+++ static/js/color.js
@@ -1,4 +1,4 @@
-var colors = [
+var colors2 = [
     '#ff0000',
     '#ff9900',
     '#ffff00',
@@ -9,4 +9,17 @@ var colors = [
     '#c27ba0',
     '#6fa8dc',
     '#d9ead3'
+]
+
+var colors = [
+    "#1f77b4",
+    "#ff7f0e",
+    "#2ca02c",
+    "#d62728",
+    "#9467bd",
+    "#8c564b",
+    "#e377c2",
+    "#7f7f7f",
+    "#bcbd22",
+    "#17becf"
 ]
